cb9a53b23c84c5a36085bb635685f6846b98e756
cjtim <47479090+cjtim@users.noreply.github.com>
2022-06-02 00:15:29 +0700

    login with line ssr

--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -3,15 +3,23 @@ import LineLoginButton from 'components/login/LineLoginButton'
 import { getLoginLink } from 'libs/auth'
 import { NextPage } from 'next'
 
-const UserLogin: NextPage = () => {
+const UserLogin: NextPage<{ loginUrl: string }> = ({ loginUrl }) => {
   const onClick = async () => {
-    window.location.href = await getLoginLink()
+    window.location.href = loginUrl
   }
   return (
     <Center top="50%">
       <LineLoginButton onClick={onClick} />
     </Center>
   )
+}
+
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      loginUrl: await getLoginLink(),
+    },
+  }
 }
 
 export default UserLogin
