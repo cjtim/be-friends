112ba74cd8b510d70d23417d575c552718e87a7f
cjtim <47479090+cjtim@users.noreply.github.com>
2022-06-02 23:55:22 +0700

    lint

--- config.ts
+++ config.ts
@@ -3,12 +3,12 @@ const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8080'
 export const config = {
   cookies: {
     token: 'authToken',
-    previousPage: 'previousPage'
+    previousPage: 'previousPage',
   },
   login: {
     GET_line: `${BACKEND_URL}/api/v1/auth/line`,
     GET_me: `${BACKEND_URL}/api/v1/auth/me`,
-    GET_line_jwt: `${BACKEND_URL}/api/v1/auth/line/jwt`
+    GET_line_jwt: `${BACKEND_URL}/api/v1/auth/line/jwt`,
   },
 }
 
