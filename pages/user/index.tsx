1393d2361bc461cdba0761b9a437fda32b129006
cjtim <47479090+cjtim@users.noreply.github.com>
2022-06-12 23:50:49 +0700

    merge master to JFP-63 (#10)
    * feat: JFP-65 register with email password (#8)
    
    * feat: register page
    
    * JFP-65 default locale to th
    
    * JFP-65 add user info component
    
    * [skip ci] [no build] Update package.json version

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,38 +1,23 @@
-import { Box, Stack, Text } from '@chakra-ui/react'
+import { Center } from '@chakra-ui/react'
 import Navbar from 'components/global/Navbar'
 import PageLayout from 'components/global/PageLayout'
+import UserInfo from 'components/login/UserInfo'
 import { AuthGetServerSideProps } from 'libs/auth'
 
 import { NextPage } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import NextImage from 'next/image'
 import { UserProps } from 'pages/_app'
 
 const User: NextPage<UserProps> = ({ user }) => (
   <PageLayout title="User Profile">
     <Navbar user={user} />
-    <Stack>
-      <Text>ID: {user?.id}</Text>
-      <Text>IMAGE: {user?.picture_url}</Text>
-      <Text>NAME: {user?.name}</Text>
-      {user?.picture_url && (
-        <Box position="relative">
-          <NextImage
-            src={user?.picture_url}
-            width="100px"
-            height="100px"
-            alt="user profile picture"
-            style={{ borderRadius: '50%' }}
-          />
-        </Box>
-      )}
-    </Stack>
+    <Center h="80vh">{user && <UserInfo user={user} />}</Center>
   </PageLayout>
 )
 
 export const getServerSideProps = AuthGetServerSideProps(async ctx => ({
   props: {
-    ...(await serverSideTranslations(ctx.locale || 'us', ['common'])),
+    ...(await serverSideTranslations(ctx.locale || 'us', ['common', 'user'])),
   },
 }))
 
